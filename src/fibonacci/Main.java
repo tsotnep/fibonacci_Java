55774336c9cb4c4606f3e3314f4f99036a2d9385
tsotne <tsotne@localhost.localdomain>
2015-11-27 15:03:36 +0200

    improvement of last version, check if number exists, this is not useful if we are usign the function - ONCE, but if we do like: System.out.println ( iterFib_N_v3(10)); System.out.println ( iterFib_N_v3(9); then it helps, because it's not calculating second one, just printing existing one

--- src/fibonacci/Main.java
+++ src/fibonacci/Main.java
@@ -8,20 +8,28 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		n_arr[1] = n_arr[2] = 1;
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		System.out.println(iterFib_N_v2(n));
+		System.out.println(iterFib_N_v3(n));
 		sc.close();
 	}
 
-	public static int iterFib_N_v2(int n) {
+	public static int iterFib_N_v3(int n) {
 		int I = 0, J = 1;
-		while (n > 0) {
-			n--;
-			J = I + J;
-			I = J - I;
+		if (n_lastN > n) {
+			return n_arr[n];
+		} else {
+			n -= n_lastN;
+			J = n_arr[n_lastN];
+			I = n_arr[n_lastN - 1];
+			while (n > 0) {
+				n--;
+				J = I + J;
+				I = J - I;
+				n_arr[J] = J;
+			}
 		}
 		return J;
 	}
-
 }
