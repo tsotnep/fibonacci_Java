88635d5712a2a739a74f02fbd1fa4e937e180c91
tsotne <tsotne@localhost.localdomain>
2015-11-27 14:16:22 +0200

    recursive fibonacci O(n)

--- src/fibonacci/Main.java
+++ src/fibonacci/Main.java
@@ -3,24 +3,26 @@ package fibonacci;
 import java.util.Scanner;
 
 public class Main {
+	public static int[] n_arr = new int[100];
+	public static int n_lastN = 2;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		n_arr[1] = n_arr[2] = 1;
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		System.out.println(recFib_2N(n));
+		System.out.println(recFib_N(n));
 		sc.close();
 	}
 
-	public static int recFib_2N(int n) {
-		if (n == 0)
-			return 0;
-		else if (n == 1)
-			return 1;
-		else if (n == 2)
-			return 1;
-		else
-			return (recFib_2N(n - 1) + recFib_2N(n - 2));
+	public static int recFib_N(int n) {
+		if (n_lastN >= n)
+			return n_arr[n];
+		else {
+			n_arr[n] = (recFib_N(n - 1) + recFib_N(n - 2));
+			n_lastN = n;
+			return n_arr[n];
+		}
 	}
 
 }
