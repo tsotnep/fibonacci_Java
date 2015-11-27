bc452594a26255557e58de1dabab951cfd2b7a52
tsotne <tsotne@localhost.localdomain>
2015-11-27 15:26:39 +0200

    added: copyright and .gitignore

--- src/fibonacci/package-info.java
+++ src/fibonacci/package-info.java
@@ -1,9 +1,19 @@
 /**
+ * @author Tsotne Putkaradze
  * 
- */
-/**
- * @author tsotne
- *
+ * 
+ * I've implemented several versions of fibonacci algorithm implementation
+ * recursive, time: O(2^n)
+ * recursive, time: O(n)
+ * iterative v1, time: O(n)
+ * iterative v2, slightly different, time: O(n)
+ * iterative v2, time: but improvements when method is used multiple times, time: O(n)
+ * 
+ * 
+ * 
+ * 
+ * usage this IP is totally free
+ * 
  */
 package fibonacci;
 
