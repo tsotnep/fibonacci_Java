6c9ab71c144440fd1acce32774337faaf83da0f0
tsotne <tsotne@localhost.localdomain>
2015-11-27 15:02:20 +0200

    iterative fibonaccy, no temporary integer used

--- src/fibonacci/Main.java
+++ src/fibonacci/Main.java
@@ -8,24 +8,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		n_arr[1] = n_arr[2] = 1;
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		System.out.println(iterFib_N(n));
 		System.out.println(iterFib_N_v2(n));
-		System.out.println(iterFib_N_v3(n));
 		sc.close();
-	}
-
-	public static int iterFib_N(int n) {
-		int i = 0, j = 1, k = 0;
-		while (n > 0) {
-			n--;
-			k = i + j;
-			i = j;
-			j = k;
-		}
-		return j;
 	}
 
 	public static int iterFib_N_v2(int n) {
@@ -38,18 +24,4 @@ public class Main {
 		return J;
 	}
 
-	public static int iterFib_N_v3(int n) {
-		int I = 0, J = 1;
-		if (n_lastN > n) {
-			return n_arr[n];
-		} else {
-			while (n > 0) {
-				n--;
-				J = I + J;
-				I = J - I;
-				n_arr[J] = J;
-			}
-		}
-		return J;
-	}
 }
